6d98a6505c1aedad32725d374546660490b54a3c
unknown <cfapecetche@gmail.com>
2023-12-13 23:50:54 -0300

    3er entrega_bis

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
-
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native'
+import Home from './src/Screens/Home'
 import ItemListCategories from './src/Screens/ItemListCategories'
+import ItemDetail from './src/Screens/ItemDetail'
+import { useState } from 'react'
+import { useFonts } from "expo-font"
 
-const App = () => {
+const  App = () => {
+  const [categorySelected,setCategorySelected] = useState("")
+  
+    
   return (
     <View style={styles.container}>
-      <ItemListCategories />
-     </View>
-  );
+      {categorySelected ?
+        <ItemListCategories category = {categorySelected}/>
+        :
+        <Home setCategorySelected={setCategorySelected}/>
+      }
+     
+    </View>
+  )
 }
 
 export default App
+
 
 const styles = StyleSheet.create({
   container: {
